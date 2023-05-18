06ef15f2f07d55c9069cc0632d311b7babeb5831
stevenAnto <ecalcinap@unsa.edu.pe>
2023-05-17 22:51:39 -0500

    todo avance

---
+++ lab01/09-hola-usuario.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#verificamos el numero de parametros
+if [ $# -le 0 ] ; then
+  echo "Hay que introducir por lo menso un parametro"
+  exit 1 #error
+fi
+
+
+for i in $@
+do
+  echo $i
+  #con grep busco al usuario ingresado como parametro
+  #  en la lista producidad por who  y lo almaceno en usuarioConetado
+  usuarioConetado=`usuarioconectado.sh $i`
+  echo "$usuarioConetado"
+  # si el length  de  usuarioConetado <=0, entonces no esta
+  #conectado, caso contrario si esta conectado
+  if [ ${#usuarioConetado} -le 0 ]; then 
+    echo "El usuario $i no  esta conectado"
+  else
+    echo "El usuario $i esta conectado"
+  fi
+done
+
